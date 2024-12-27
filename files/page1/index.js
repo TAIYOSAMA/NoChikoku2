ef6b63680f14add11bc40588bb53b3bffc8ff223
TAIYOSAMA <85349211+TAIYOSAMA@users.noreply.github.com>
2024-12-28 08:24:03 +0900

    Update index.js

--- files/page1/index.js
+++ files/page1/index.js
@@ -2,5 +2,5 @@ const GoToPage2 = function() {
   window.location = '../page2'
 };
 const GoToNewVersion = () => {
-  window.location = 'https://taiyosama.github.io/NoChikoku3/';
+  window.open('https://taiyosama.github.io/NoChikoku3/', '_blank');
 }
